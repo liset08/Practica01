dcf956534964691c1c0ecfc04dc28df6dd4b652a
liset08 <liset.cayhualla@tecsup.edu.pe>
2018-03-05 12:42:36 -0500

    brunch

--- src/main/java/com/tecsup/Practica01/SeccionBebes.java
+++ src/main/java/com/tecsup/Practica01/SeccionBebes.java
@@ -13,6 +13,17 @@ public void name() {
 	array1[1]= "Casaca de cuero ";
 	array1[2]= "14";
 System.out.println("Nombre : " + array1[0] +"\n"+ "Descripción : "+array1[1]+"\n"+ "Talla : "  +array1[2]);
+
+
+String array2[];
+
+array2= new String[3];
+
+array2[0]= "Conjunto";
+array2[1]= "Color rosado ";
+array2[2]= "14";
+System.out.println("Nombre : " + array1[0] +"\n"+ "Descripción : "+array1[1]+"\n"+ "Talla : "  +array1[2]);
+
 	
 }	
 	
